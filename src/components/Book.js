b931f8077fb6b9648a3a999172a675a94663366a
mellaniesoriano <msoriano808@gmail.com>
2017-08-26 16:29:33 -1000

    changed into list items

--- src/components/Book.js
+++ src/components/Book.js
@@ -2,9 +2,11 @@
 import React from 'react';
 
 const Book = ({ title, author }) =>
-  <div>
-    <h3>{ title }</h3>
-    <p>{ author }</p>
-  </div>;
+  <ul>
+    <li>
+      <h3>{ title }</h3>
+      <p>{ author }</p>
+    </li>
+  </ul>;
 
 export default Book;
